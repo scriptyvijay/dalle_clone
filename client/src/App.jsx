0c072cea609f0fcf16e64f48822c8bceb407765e
Vijay Choudhary <scriptyvijay@gmail.com>
2023-05-29 20:14:03 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
 	return (
 		<div>
-			<BrowserRouter basename="/dalle">
+			<BrowserRouter>
 				<header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#e6ebf4]">
 					<Link to="/">
 						<img src={logo} alt="logo" className="w-28 object-contain" />
